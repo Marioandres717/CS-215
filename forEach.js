36ad856219db5d1f7fae5e04fda43fbad7b2d4de
Mario Andres <marioarendon2@gmail.com>
2017-12-25 19:23:18 -0600

    Update forEach.js

--- forEach.js
+++ forEach.js
@@ -1,4 +1,13 @@
 /*
+	Basic implementation of forEach
+*/
+function forEach(arr, callback){
+	for(var i = 0; i < arr.length; i++){
+		callback(arr[i], i, arr);
+	}
+}
+
+/*
 Write a function called doubleValues which accepts an array and returns a new array with all the values in the array passed to the function doubled
 
 Examples:
